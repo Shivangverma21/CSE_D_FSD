b9c818de7c4d0cf37fa1e6b34e003ec25aca2420
shivang <shivangverma8860@gmail.com>
2025-09-09 11:42:23 +0530

    Your commit message

--- college.cpp
+++ college.cpp
@@ -26,10 +26,62 @@
 // }
 
 
-#include<iostream>
-using namespace std;
-int main(){
-    for( ; ; ){
-        cout<<"elements";
-    }
-}
+// #include<iostream>
+// #include <iomanip>
+// using namespace std;
+// int main(){
+//     float a = 20.9898;
+//     cout << fixed << setprecision(6) << 10.45 << " ";
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a=3;
+//     int b=3;
+//     int c=3;
+//     if((a+b)/2 >= c){
+//         cout<<"true";
+//     }else{
+//         cout<<"false";
+//     }
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     string s="shi#an&";
+//     if(s.length()%2==0){
+//         cout<<"true";
+//     }else{
+//         cout<<"false";
+//     }
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+//     int n = sizeof(arr)/sizeof(arr[0]);
+//     int evenCount = 0, oddCount = 0;
+//     for(int i = 0; i < n; i++) {
+//         if(arr[i] % 2 == 0)
+//             evenCount++;
+//         else
+//             oddCount++;
+//     }
+//     if(evenCount > oddCount)
+//         cout << "even";
+//     else if(oddCount > evenCount)
+//         cout << "odd";
+//     else
+//         cout << "tie";
+
+//     return 0;
+// }
+
