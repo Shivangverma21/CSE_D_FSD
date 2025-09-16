898e218d5de62ad5389871e116afa7b43fdac8c8
shivang <shivangverma8860@gmail.com>
2025-09-16 12:23:12 +0530

    Your commit message

---
+++ CSE_D_FSD/First.js
@@ -0,0 +1,4 @@
+let x=10;
+let y=10;
+console.log("sum of x and y is:",x+y);
+console.log("First JS");
--- college.cpp
+++ college.cpp
@@ -85,3 +85,50 @@
 //     return 0;
 // }
 
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
+// int main() {
+//     int n;
+//     cout << "Enter number of elements: ";
+//     cin >> n;
+//     vector<int> arr(n);
+//     cout << "Enter array elements: ";
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+//     int a = count(arr.begin(), arr.end(), 1);
+//     cout << "Number of 1's: " << a << endl;
+//     auto it = find(arr.begin(), arr.end(), 1);
+//     if (it != arr.end()) {
+//         arr.erase(it);
+//     }
+//     cout << "Array after erasing first occurrence of 1: ";
+//     for (int i = 0; i < arr.size(); i++) {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+//     return 0;
+// }
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>v1={2,6,1,3,9};
+    vector<int>v2={5,1,8,7,0,4,1,2};
+    for(int i=0;i<v1.size();i++){
+        for(int j=0;j<v2.size();j++){
+            if(v1[i]>v2[j]){
+                cout<<v1[i];
+            }else{
+                cout<<j;
+            }
+        }
+        return 0;
+    }
+}
+
+
+
